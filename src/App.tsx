614eb0fd6f30addfbb1a80b3300593bd13ba1131
Arthur-DevionFox <blackberserkfox@mail.ru>
2026-03-20 11:56:26 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { TaskCard } from './components/TaskCard/TaskCard'
+import type { Task } from './types/task';
+
+function App() {
+
+const tasks: Task = 
+  {
+    title: "Настроить авторизацию",
+    description: "Реализовать вход по email и паролю с валидацией",
+    priority: "low",
+    status: "todo",
+  }
+;
+
+  return (
+    <>
+      <section id="center">
+        <div className='app'>
+          <h1 className='app-title'>Task Card</h1>
+        </div>
+        <TaskCard task={tasks}/>
+      </section>
+    </>
+  )
+}
+
+export default App
